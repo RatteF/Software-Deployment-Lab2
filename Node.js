268235ed8d750c2dded9e0aa3529e29ca1370bc3
RatteF <83348757+RatteF@users.noreply.github.com>
2023-10-25 20:59:05 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -3,9 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hallo, wenn diese Nachricht kommt, hat der Test funktioniert.');
+  res.send(getResponse());
 });
 
 app.listen(port, () => {
   console.log(`Die Anwendung ist auf Port ${port} gestartet.`);
 });
+
+function getResponse() {
+  return 'Hallo, wenn diese Nachricht kommt, hat der Test funktioniert.';
+}
